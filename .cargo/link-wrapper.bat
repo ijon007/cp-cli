aec4502619b8a326d25ba8d2f85dbf2fbe5ba66e
codify4 <kushta.joni@gmail.com>
2026-02-04 20:33:42 +0100

    chore: remove .cargo from tracking

--- .cargo/link-wrapper.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-call "C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvars64.bat"
-"C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Tools\MSVC\14.44.35207\bin\Hostx64\x64\link.exe" %*
